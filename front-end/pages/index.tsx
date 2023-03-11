b99322eea8016583fa43fdfd05770e55af935abf
Ümit Gül <uemitgul@gmail.com>
2023-03-11 15:50:41 +0100

    new layout for tabbar and center wallet

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,22 +1,37 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
-import { SimpleGrid, Container, Heading,Stack, Divider,  } from '@chakra-ui/react'
+import { SimpleGrid, Container, Heading, Stack, HStack, Divider, Tabs, Tab, TabList, TabPanels, TabPanel  } from '@chakra-ui/react'
 import ActiveCard from '../components/activeCard';
+import InActiveCard from '../components/inActiveCard';
 
 
 const Home: NextPage = () => {
   return (
     <Container maxW="container.xl">
-    <ConnectButton />
-      <Stack spacing={1}>
-        <Heading>Active Memberships</Heading>
-        <Divider />
-        <SimpleGrid minChildWidth="350px" spacing={8} py={8}>
-          <ActiveCard />
-          <ActiveCard />
-        </SimpleGrid>
-      </Stack>
-      
+      <Container p={'16'} centerContent> 
+      <ConnectButton /> 
+      </Container>
+    
+      <Tabs isFitted size='lg' variant='solid-rounded'>
+        <TabList mb='1em'>
+          <Tab>Inactive NFTs</Tab>
+          <Tab>Active NFTs</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SimpleGrid minChildWidth="350px" spacing={8} py={8}>
+              <InActiveCard />
+              <InActiveCard />
+            </SimpleGrid>
+          </TabPanel>
+          <TabPanel>
+            <SimpleGrid minChildWidth="350px" spacing={8} py={8}>
+              <ActiveCard />
+              <ActiveCard />
+            </SimpleGrid>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>  
     </Container>
   );
 };
